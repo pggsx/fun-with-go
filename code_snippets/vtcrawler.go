cc28bcf353a5f648fc262f3ad97c054721a7629f
Kyle Ames <KyleAmes13@gmail.com>
2016-01-24 12:48:14 -0500

    Performed code review. Comments start with "KEA"

--- code_snippets/vtcrawler.go
+++ code_snippets/vtcrawler.go
@@ -5,18 +5,52 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/williballenthin/govt"
 	"io/ioutil"
 	"net"
 	"os"
 	"strings"
+
+	"github.com/williballenthin/govt"
 )
+
+// KEA -Go uses camelCase instead of spacing_like_this
+
+/*
+  KEA - A good design would be to populate a []string of URL's in your main function. Then just loop and call fetch. It would clean up your "if cli_str/cli_file" logic, and allow you to get rid of the if/else. You could simply append to the []string if the flag is given. Nice, clean and concise.
+
+  Pseudocode:
+
+  urls := make([]string, 0)
+
+  concurrent := flag.Bool("j", false, "Concurrently fetch url reports")
+
+  if *cli_str != "" {
+      // Do whatever validity checks you want
+      urls = append(urls, *cli_str)
+  }
+
+  if *cli_file != "" {
+      // Open file
+      // For line in file - urls = append(urls, *cli_str)
+  }
+
+  for _, url := range urls {
+      if *concurrent {
+	go fetch()
+      } else {
+	fetch()
+      }
+  }
+*/
 
 var apikey string
 var apiurl string
 var domain string
 var ip string
 
+/*
+KEA - Init should only be used for initializing packages (init is called on import). It should not be used for your main functions. (the reason is mainly idiomatic, but also semantic)
+*/
 //Initialization Function
 func init() {
 	flag.StringVar(&apikey, "apikey", os.Getenv("VT_API_KEY"), "VirusTotal APIK")
@@ -30,8 +64,22 @@ func main() {
 	var scan_url string
 	var verbose bool
 	var silent bool
-	//	var counter int
+
+	/*
+		KEA - In Go, prefer Slices to Arrays. Do this instead
+
+		urls := make([]string, 0)
+
+		This creates a slice of strings of length 0 and capacity 0. Later in your code instead of doing url_array[i] = <some string>, do urls = append(urls, <some string>)
+
+		The append function will handle growing and memcopying the slice for you as needed. You'd normally want to do urls := make([]string, 0, <how many you know you'll need>) to avoid the alloc and memcopy, but in this case you don't know so this is ok.
+	*/
 	var url_array [100]string
+
+	/*
+		KEA - It's idiotmatic to leave the var out and do
+		cli_str := flag.String("u", "", "Individual URL to be scanned")
+	*/
 	var cli_str = flag.String("u", "", "Individual URL to be scanned")
 	var cli_verb = flag.String("m", "v,s", "Verbose Mode (v) or Silent Mode (s)")
 	var cli_file = flag.String("i", "urls.txt", "File of URLs to be scanned")
@@ -40,8 +88,18 @@ func main() {
 
 	fmt.Println("VT URl Scanner")
 
+	/*
+		KEA - The check on the length is not neccessary. If no flag is provided, it is set to the default you gave the flag call , which is the empty string.
+	*/
 	if *cli_str != "" && len(*cli_str) > 0 {
 		scan_url = *cli_str
+
+		/*
+			KEA - Better here to define two separate flags instead of combining it under cli_verb.
+
+			silent := flag.Bool("s", false, "Enable silent mode")
+			verbose := flag.Bool("v", false, "Enable verbose mode")
+		*/
 		if strings.EqualFold(*cli_verb, "v") {
 			fmt.Println("Verbose Mode Selected")
 			verbose = true
@@ -51,14 +109,14 @@ func main() {
 			silent = true
 			fetch(scan_url, silent)
 		}
-	} else if *cli_file != "" && len(*cli_file) > 0 {
+	} else if *cli_file != "" && len(*cli_file) > 0 { // KEA: Same here of course with the length check
 
 		file, err := os.Open("urls.txt")
 		if err != nil {
 			return
 		}
 		defer file.Close()
-		scanner := bufio.NewScanner(file)
+		scanner := bufio.NewScanner(file) // KEA: Love it, nice use of the fact that os.File is an io.Reader. Well done.
 		ii = 0
 		for scanner.Scan() {
 			url_array[ii] = scanner.Text()
@@ -113,13 +171,28 @@ func fetch(url string, mode bool) {
 	// get a file report
 	r, err := c.GetIpReport(ip)
 	check(err)
+
+	// KEA - Careful, you're swalloing the json.MarshalIndent error here. Always always always error check
 	j, err := json.MarshalIndent(r, "", "    ")
+
+	/*
+		KEA
+
+		Idiomatic Go prefers handling errors in the indented blocks and keeping the normal code flow as unindented as possible. Really helps readability and flow.
+
+		rc, err := SomeFunc()
+		if err != nil {
+		  // Error Handle
+		}
+
+		// Normal Code Path
+	*/
 	if _, err := os.Stat(file); err == nil {
 		fmt.Println("File Exists")
 		ioutil.WriteFile("test.txt", j, 0664)
 	}
-	ioutil.WriteFile("report.txt", j, 0664)
-	check(err)
+	ioutil.WriteFile("report.txt", j, 0664) // KEA - Don't swallow errors.  err := ioutil.WriteFile
+	check(err)                              // KEA - This is the json.MarshalIndent err currently
 
 	if mode {
 		fmt.Print("Report Generated\n")
@@ -127,17 +200,20 @@ func fetch(url string, mode bool) {
 	}
 }
 
+// KEA - You'll instead be able to pass a slice here. This is massively more efficient as when you pass an array go must copy the array on the runtime stack for the function call. With a slice it simply puts the reference on the stack.
 func multi_fetch(url [100]string, mode_str string, size int) {
 	fmt.Println("inside of multi_fetch")
 	var aa int
 	var mode bool
 	mode = false
 
+	// KEA - Check this explicitly with bool vars, string comparison is error prone and easy to break.
 	if strings.EqualFold(mode_str, "verbose") {
 		mode = true
 	}
 	for aa = 0; aa < size; aa++ {
 		if url[aa] != "" {
+			// KEA - Aha, the problem you are having with the goroutines is that each one is going to try to do something with "report.txt". If you make it so that the filename is different every time somehow (perhaps a random number?) then your concurrent code will be ok.
 			go fetch(url[aa], mode)
 		}
 	}
@@ -145,6 +221,10 @@ func multi_fetch(url [100]string, mode_str string, size int) {
 }
 
 // check - an error checking function
+
+/*
+KEA - Never panic unless there's a really really good reason to. Better to log and then os.Exit, or log.Fatal. Go isn't an exception driven language.
+*/
 func check(e error) {
 	if e != nil {
 		panic(e)
